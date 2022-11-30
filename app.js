2ee690bd0c2c071d578d9657802bf1d42de45d81
Sergiy Pazyuk <pazyk.15@gmail.com>
2022-11-30 21:59:29 +0200

    lab4 - body parser

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ import express from 'express';
 import userRouter from './routes/userRoute.js';
 import _ from 'lodash';
 import morgan from 'morgan';
+import bodyParser from 'body-parser';
 
 const SERVER_PORT = 3001;
 const USERS_URL = '/api/v1/users';
 
 const server = express();
 
+// middleware
 server.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+server.use(bodyParser.json());
 server.use(USERS_URL, userRouter);
+// middleware
 
+// default routes
 server.get('/', (req, res) => {
     console.log("Hello from root");
     res.send("Hello browser!");
@@ -19,3 +24,5 @@ server.get('/', (req, res) => {
 server.listen(SERVER_PORT, () => {
     console.log(`Server listen on port ${SERVER_PORT}`)
 })
+// default routes
+
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,20 +2,34 @@ import express from 'express';
 import _ from 'lodash';
 import users from '../data/users.json';
 
+let usersStorage = _.toArray(users);
+
 const userRouter = express.Router();
 
 userRouter.get('/', (req, res) => {
-    res.json(users);
+    res.json(usersStorage);
 })
 
 userRouter.get(`/:id`, (req, res) => {
     const id = req.params.id;
-    const user = _.find(users, user => user.id === id);
+    const user = _.find(usersStorage, user => user.id === id);
     if (user) {
         res.json(user);
     } else {
         res.send("Not found");
     }
+})
+
+userRouter.post("/", (req, res) => {
+    const userRequest = req.body;
+    if (userRequest && userRequest.username && userRequest.email && userRequest.password) {
+        usersStorage.push({
+            ...userRequest,
+            id: (usersStorage.length + 1).toString()
+        });
+        return res.send("201 Created")
+    }
+    return res.send("403 Bad request")
 })
 
 export default userRouter;
